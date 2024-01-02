9beb33a5165f2837230be2580facccc91675e7a4
Veniamin Ilmer <ven@ven.com>
2024-01-02 10:07:56 -0500

    forgot to include binary

---
+++ pkg/hp80_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run(): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h0f94264175d0222f(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h6e347dbe5e441f0d(a: number, b: number, c: number, d: number): void;
